4508d0b760a35f8d8eb9d3f17a7c9a9397c75a1a
N1ve5h <niveshg@hotmail.com>
2021-08-18 11:43:46 +0100

    more for ofs

---
+++ Javascript/for-of2.js
@@ -0,0 +1,12 @@
+const seatingChart = [
+    ['Kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+
+for (let row of seatingChart) {
+
+    for (let name of row) {
+        console.log(name)
+    }
+}
