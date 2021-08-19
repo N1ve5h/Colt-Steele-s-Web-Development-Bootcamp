8381256e816d849a01efe66047a4ea6ad6c87035
N1ve5h <niveshg@hotmail.com>
2021-08-19 13:13:08 +0100

    Return function

---
+++ Javascript/return.js
@@ -0,0 +1,7 @@
+function square(x) {
+  return x * x; //Return function can only return ONE value.
+}
+
+var demo = square(3); //The return value is stored in a variable outside the function.
+
+console.log(demo);
