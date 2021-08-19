1e1d4effd01da45a730162e522151a3431d7ed73
N1ve5h <niveshg@hotmail.com>
2021-08-19 10:33:30 +0100

    To Do List

---
+++ Javascript/To Do List/todos.js
@@ -0,0 +1,42 @@
+let input = prompt ('What would you like to do?');
+const todos = ['Collect Chicken Eggs', 'Clean Cat Litter'];
+
+while (input !== 'quit' && input !== 'q') {
+    
+    if (input === 'list') {
+
+        console.log('***************');
+
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+
+        console.log('***************');
+
+    } else if (input === 'new') {
+        
+        const  newTodo = prompt('Ok, what is the new todo?');
+        
+        todos.push(newTodo);
+
+        console.log(`${newTodo} added to the list`);
+
+    } else if (input === 'delete') {
+        
+        const index = parseInt(prompt('Enter an index to delete: '));
+        
+        if (!Number.isNaN(index)) {
+            
+            const deleted = todos.splice(index,1);
+
+            console.log(`Ok, deleted ${deleted[0]}`);
+        } else {
+            
+            console.log('Unknown Index')
+        }
+        
+    }
+    input = prompt ("What would you like to do?");
+}
+
+console.log("OK, YOU QUIT THE APP!");
