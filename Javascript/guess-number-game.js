5157b2512ef1ce51ace4a45787ae5fc0947516e1
N1ve5h <niveshg@hotmail.com>
2021-08-19 12:48:34 +0100

    untitled

--- Javascript/guess-number-game.js
+++ Javascript/guess-number-game.js
@@ -1,28 +1,28 @@
 let maximum = parseInt(prompt("Enter the maximum number!"));
 
 while (!maximum) {
-    maximum = parseInt(prompt("Enter the maximum number!"));
+  maximum = parseInt(prompt("Enter the maximum number!"));
 }
 
-console.log(maximum)
+console.log(maximum);
 
-const targetNum = Math.floor(Math.random()* maximum)+1;
+const targetNum = Math.floor(Math.random() * maximum) + 1;
 
 let guess = parseInt(prompt("Enter your guess!"));
 let attempts = 1;
 
 while (parseInt(guess) !== targetNum) {
-    if (guess === 'q') break;
-    attempts++;
-    
-    if (guess > targetNum) {
-        guess = prompt("TOO HIGH, Enter your guess!");
-    } else {
-        guess = prompt("TOO LOW, Enter your guess!");
-    }
+  if (guess === "q") break;
+  attempts++;
+
+  if (guess > targetNum) {
+    guess = prompt("TOO HIGH, Enter your guess!");
+  } else {
+    guess = prompt("TOO LOW, Enter your guess!");
+  }
 }
-if (guess === 'q') {
-    console.log("YOU QUIT!")
+if (guess === "q") {
+  console.log("YOU QUIT!");
 } else {
-    console.log("You got it! It took you "+attempts+"  guesses")
+  console.log("You got it! It took you " + attempts + "  guesses");
 }
