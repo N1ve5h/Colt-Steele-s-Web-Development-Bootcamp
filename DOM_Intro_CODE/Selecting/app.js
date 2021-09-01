a8753d277211f6111819fd445cd159ee554d71e0
N1ve5h <niveshg@hotmail.com>
2021-09-01 12:49:01 +0100

    Code Along task

--- DOM_Intro_CODE/Selecting/app.js
+++ DOM_Intro_CODE/Selecting/app.js
@@ -1,18 +1,20 @@
-const allImages = document.getElementsByTagName('img');
+// const allImages = document.getElementsByTagName("img");
 
-for (let img of allImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
-}
+// for (let img of allImages) {
+//   img.src =
+//     "https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80";
+// }
+// Change all images to have the same src.
 
+// const squareImages = document.getElementsByClassName("square");
 
-const squareImages = document.getElementsByClassName('square');
+// for (let img of squareImages) {
+//   img.src =
+//     "https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80";
+// }
 
-for (let img of squareImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
-}
-
-const links = document.querySelectorAll('p a');
+const links = document.querySelectorAll("p a");
 
 for (let link of links) {
-    console.log(link.href)
+  console.log(link.href);
 }
