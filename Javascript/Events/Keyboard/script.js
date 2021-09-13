99634627ff02b5f841f5e08bf60017fbd4070a9c
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2021-09-13 10:03:59 +0100

    console log arrow key

--- Javascript/Events/Keyboard/script.js
+++ Javascript/Events/Keyboard/script.js
@@ -4,9 +4,30 @@ document.querySelector("button").addEventListener("click", function (evt) {
 
 const input = document.querySelector("input");
 
-input.addEventListener("keydown", function () {
-  console.log("KEYDOWN");
-});
-input.addEventListener("keyup", function () {
-  console.log("KEYUP");
-});
+// input.addEventListener("keydown", function (e) {
+//   console.log(e.key); just name
+//   console.log(e.code); location of the key on the keyboard
+// });
+// input.addEventListener("keyup", function () {
+//   console.log("KEYUP");
+// });
+
+window.addEventListener('keydown',function (e){
+  switch (e.code) {
+    case 'ArrowUp':
+      console.log("UP!");
+      break;
+    case 'ArrowDown':
+      console.log("DOWN!");
+      break;
+    case 'ArrowLeft':
+      console.log("LEFT!");
+      break;
+    case 'ArrowRight':
+      console.log("RIGHT!");
+      break;
+    default:
+      console.log("IGNORED");
+      
+  }
+})
