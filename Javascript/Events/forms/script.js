8ca1fd4364963eab4bcc3a2f715777daed47cc5f
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2021-09-13 11:51:49 +0100

    post comment locally

---
+++ Javascript/Events/forms/script.js
@@ -0,0 +1,21 @@
+const commentForm = document.querySelector('#commentForm');
+const commentContainer = document.querySelector('#comments')
+
+commentForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const username = commentForm.elements.username;
+    const comment = commentForm.elements.comment;
+    
+    addComment(username.value,comment.value);
+    username.value='';
+    comment.value='';
+})
+
+const addComment = (username,comment) => {
+     const newComment = document.createElement("li");
+     const bolding = document.createElement("b");
+     bolding.append(username);
+     newComment.append(bolding);
+     newComment.append(` - ${comment}`);
+     commentContainer.append(newComment);
+}
