0ad186d5a307ee1863867f67914773f3389fa681
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2021-09-22 09:49:38 +0100

    nested callbacks

--- Javascript/PromisesIntro/script.js
+++ Javascript/PromisesIntro/script.js
@@ -1,30 +1,57 @@
 // THE CALLBACK VERSION
 const fakeRequestCallback = (url, success, failure) => {
+  const delay = Math.floor(Math.random() * 4500) + 500;
+  setTimeout(() => {
+    if (delay > 4000) {
+      failure("Connection Timeout :(");
+    } else {
+      success(`Here is your fake data from ${url}`);
+    }
+  }, delay);
+};
+// THE PROMISE VERSION
+const fakeRequestPromise = (url) => {
+  return new Promise((resolve, reject) => {
     const delay = Math.floor(Math.random() * 4500) + 500;
     setTimeout(() => {
-        if (delay > 4000) {
-            failure('Connection Timeout :(')
-        } else {
-            success(`Here is your fake data from ${url}`)
-        }
-    }, delay)
-}
-// THE PROMISE VERSION 
-const fakeRequestPromise = (url) => {
-    return new Promise((resolve, reject) => {
-        const delay = Math.floor(Math.random() * (4500)) + 500;
-        setTimeout(() => {
-            if (delay > 4000) {
-                reject('Connection Timeout :(')
-            } else {
-                resolve(`Here is your fake data from ${url}`)
-            }
-        }, delay)
-    })
-}
+      if (delay > 4000) {
+        reject("Connection Timeout :(");
+      } else {
+        resolve(`Here is your fake data from ${url}`);
+      }
+    }, delay);
+  });
+};
 
-fakeRequestCallback('books.com', function (){
+//Fake CallbackHell
+fakeRequestCallback(
+  "books.com/page1",
+  function (response) {
     console.log("It Worked!");
-}, function () {
-    console.log("It Failed!");
-})
+    console.log(response);
+    fakeRequestCallback(
+      "books.com/page2",
+      function (response) {
+        console.log("It Worked Again!");
+        console.log(response);
+        fakeRequestCallback(
+          "books.com/page3",
+          function (response) {
+            console.log("It Worked!");
+            console.log(response);
+          },
+          function (err) {
+            console.log("It Failed!", err);
+          }
+        );
+      },
+      function (err) {
+        console.log("It Failed!", err);
+      }
+    );
+  },
+  function (err) {
+    console.log("It Failed!", err);
+  }
+);
+
