a102cf2a0f4fdf8a2fec1719a5f99719f852ef89
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2021-09-24 11:20:07 +0100

    using await for async

--- Javascript/AyncFunction2/script.js
+++ Javascript/AyncFunction2/script.js
@@ -1,24 +1,51 @@
-const delayedColorChange = (color, delay) => {
-    return new Promise ((resolve, reject) => {
+// const delayedColorChange = (color, delay) => {
+//     return new Promise ((resolve, reject) => {
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = color;
+//             resolve();
+//         }, delay);
+//     })
+// }
+
+// async function rainbow() {
+//     await delayedColorChange('red',1000);
+//     await delayedColorChange('orange',1000);
+//     await delayedColorChange('yellow', 1000);
+//     await delayedColorChange('green', 1000);
+//     await delayedColorChange('blue', 1000);
+//     await delayedColorChange('indigo', 1000);
+//     await delayedColorChange('violet', 1000);
+//     return "All Done";
+// }
+
+// async function printRainbow() {
+//     await rainbow();
+//     console.log("Rainbow Completed");
+// }
+
+
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject)  => {
+        const delay = Math.floor(Math.random()*(4500) + 500);
         setTimeout(() => {
-            document.body.style.backgroundColor = color;
-            resolve();
+            if (delay > 4000) {
+                reject('Connection Timeout')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
         }, delay);
     })
 }
 
-async function rainbow() {
-    await delayedColorChange('red',1000);
-    await delayedColorChange('orange',1000);
-    await delayedColorChange('yellow', 1000);
-    await delayedColorChange('green', 1000);
-    await delayedColorChange('blue', 1000);
-    await delayedColorChange('indigo', 1000);
-    await delayedColorChange('violet', 1000);
-    return "All Done";
-}
-
-async function printRainbow() {
-    await rainbow();
-    console.log("Rainbow Completed");
+const makeTwoRequests = async () => {
+    try {
+        let data1 = await fakeRequest('/page1');
+        console.log(data1);
+        let data2 = await fakeRequest('/page2');
+        console.log(data2);
+    } catch (e) {
+        console.log("Caught An Error!");
+        console.log(e);
+    }
+    
 }
