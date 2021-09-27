3e454cd47dbc29e23519dbcb5c24ec7ae314de4e
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2021-09-27 12:51:30 +0100

    fetch and axios get

--- Javascript/Fetch-Axios/script.js
+++ Javascript/Fetch-Axios/script.js
@@ -43,9 +43,20 @@
 //         console.log("Error ", e)
 //     }
 // }
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('.button');
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLi = document.createElement('li');
+    newLi.append(jokeText);
+    jokes.append(newLi);
+}
 
 const getDadJoke = async () => {
     const config = { headers: { Accept : 'application/json'}};
     const res = await axios.get('https://icanhazdadjoke.com',config);
-    console.log(res);
+    return res.data.joke;
 }
+
+button.addEventListener('click', addNewJoke);
