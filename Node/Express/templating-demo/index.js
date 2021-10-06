7eabe78536d7839bbc74747d77bf2983f5f433c6
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2021-10-06 11:57:21 +0100

    Passing random variable

--- Node/Express/templating-demo/index.js
+++ Node/Express/templating-demo/index.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
+app.get('/rand', (req, res) => {
+    const num = Math.round(Math.random()*10)+1;
+    res.render('random',{ num } );
+})
+
 app.listen(8080, () => {
     console.log("Listening On Port 8080");
 })
