50db4171ad51225487522e5c72426cf61afb114e
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2021-10-14 09:14:29 +0100

    Subreddits using data

--- Node/Express/templating-demo/index.js
+++ Node/Express/templating-demo/index.js
@@ -2,12 +2,25 @@ const express = require("express");
 const { join } = require("path");
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
 
 app.set('view engine', 'ejs');
 app.set('views', join(__dirname,'/views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+})
+
+app.get('/cats', (req,res) =>{
+    const cats = ['Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'];
+    res.render('cats', {cats})
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const {subreddit} = req.params;
+    const data = redditData[subreddit];
+    console.log(data)
+    res.render('subreddit', { ...data })
 })
 
 app.get('/rand', (req, res) => {
