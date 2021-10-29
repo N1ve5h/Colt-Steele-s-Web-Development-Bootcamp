92a4b5ae992c5ef3574760e725a44340459bd93d
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2021-10-29 12:56:11 +0100

    Spelling

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -21,7 +21,7 @@ const Movie = mongoose.model('Movie', movieSchema); //create model
 // const amadeus = new Movie({title:'Amadeus', year: 1986, score:9.2, rating: 'R'});
 
 // Movie.insertMany([
-//   { title: 'Amelue', year: 2001, score: 8.3, rating: 'R'},
+//   { title: 'Amelie', year: 2001, score: 8.3, rating: 'R'},
 //   { title: 'Alien', year: 1979, score: 8.1, rating: 'R'},
 //   { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG'},
 //   { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R'},
