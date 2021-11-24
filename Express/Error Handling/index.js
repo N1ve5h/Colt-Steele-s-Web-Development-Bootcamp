38f5c57460bede0b89e35e5770de287dd3e56052
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2021-11-24 12:38:21 +0000

    password and error

--- Express/Error Handling/index.js
+++ Express/Error Handling/index.js
@@ -1,37 +1,59 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const AppError = require("./AppError");
 
 app.use(morgan("tiny"));
 
 app.use((req, res, next) => {
-    req.requestTime = Date.now();
-    // console.log(req.method, req.path, req.requestTime);
-    next();
-})
+  req.requestTime = Date.now();
+  // console.log(req.method, req.path, req.requestTime);
+  next();
+});
 
-const verifyPassword  = (req,res,next) => {
-    const { password } = req.query;
-    if (password === 'chickennugget') {
-      return next();
-    }
-    res.send('WRONG PASSWORD TRY AGAIN');
-}
+const verifyPassword = (req, res, next) => {
+  const { password } = req.query;
+  if (password === "chickennugget") {
+    return next();
+  }
+  // res.send("WRONG PASSWORD TRY AGAIN");
+  throw new AppError("Password required", 401);
+};
 
 app.get("/", (req, res) => {
   res.send("Homepage!");
 });
 
+app.get("/error", (req, res) => {
+  chicken.fly();
+});
+
 app.get("/dogs", (req, res) => {
   res.send("WOOF WOOF");
+});
+
+app.get("/secret", verifyPassword, (req, res) => {
+  res.send("My Secret is: I believe python is psuedo-code.");
+});
+
+app.get('/admin', (req, res) => {
+  throw new AppError('You are not an Admin', 403)
 })
 
-app.get("/secret",verifyPassword, (req,res) => {
-  res.send("My Secret is: I believe python is psuedo-code.")
-})
+app.use((req, res) => {
+  res.status(404).send("NOT FOUND");
+});
 
-app.use((req,res) => {
-    res.status(404).send("NOT FOUND")
+// app.use((err, req, res, next) => {
+//   console.log("*******************************");
+//   console.log("*************ERROR*************");
+//   console.log("*******************************");
+//   next(err);
+// })
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Something went wrong!'} = err
+  res.status(status).send(message);
 })
 
 app.listen(3000, () => {
