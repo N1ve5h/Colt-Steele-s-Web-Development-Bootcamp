881add6abe5936bc9214620cce96edf2ce52cd7e
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2021-11-24 13:34:33 +0000

    product schema

---
+++ Express/ErrorHandlingAsync/seeds.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const Product = require("./product");
+
+mongoose
+  .connect("mongodb://localhost:27017/farmStand2")
+  .then(() => {
+    console.log("Connected to Database");
+  })
+  .catch((err) => {
+    console.log("Can not connect to Database");
+    console.log(err);
+  });
+
+// const p = new Product({
+//   name: "Ruby Grapefruit",
+//   price: 1.99,
+//   category: "fruit",
+// });
+
+// p.save().then((p) => {
+//   console.log(p);
+// })
+// .catch(e => {
+//     console.log("Error");
+//     console.log(e);
+// })
+
+const seedProducts = [
+    {
+        name: 'Fairy Eggplant',
+        price: 1.00,
+        category: 'vegetable',
+    },
+    {
+        name: 'Organic Goddess Melon',
+        price: 4.99,
+        category: 'fruit',
+    },
+    {
+        name: 'Organic Mini Seedless Watermelon',
+        price: 3.99,
+        category: 'fruit',
+    },
+    {
+        name: 'Organic Celery',
+        price: 1.50,
+        category: 'vegetable',
+    },
+    {
+        name: 'Chocolate Whole Milk',
+        price: 2.69,
+        category: 'dairy',
+    },
+];
+
+Product.insertMany(seedProducts).then(() => console.log("Data saved")).catch(() => console.log("Error"))
