0d10abf6cf1805cda402afe5bb5a4b399566114a
N1ve5h <46673471+N1ve5h@users.noreply.github.com>
2022-01-10 10:24:50 +0000

    new products for farms

--- Mongo_Relationships-Express/index.js
+++ Mongo_Relationships-Express/index.js
@@ -41,8 +41,24 @@ app.get("/farms/:id", async (req, res) => {
 app.post("/farms", async (req, res) => {
   const farm = new Farm(req.body);
   await farm.save();
-  res.redirect("farms");
+  res.redirect("/farms");
 });
+
+app.get('/farms/:id/products/new', (req, res) => {
+  const { id } = req.params;
+  res.render('products/new', {categories, id})
+});
+
+app.post('/farms/:id/products', async (req,res) => {
+  const { id } = req.params;
+  const farm = await Farm.findById(id);
+  const product = new Product(req.body);
+  farm.products.push(product);
+  product.farm = farm;
+  await farm.save();
+  await product.save();
+  res.send(farm);
+})
 
 //Product Routes
 const categories = ["fruit", "vegetable", "dairy"];
